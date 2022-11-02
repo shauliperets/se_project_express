140a51134f9221a9b0afae551d2d28b2953ffb4f
shauliperets <shauliperets@gmail.com>
2022-11-02 11:57:23 +0200

    Routes work for user id

--- routes/cards.js
+++ routes/cards.js
@@ -7,15 +7,17 @@ const fsPromises = require("fs").promises;
 
 const CARDS_PATH = path.join(__dirname, "../data/cards.json");
 
-console.log("CARD_PATH =>", CARDS_PATH);
+//console.log("CARD_PATH =>", CARDS_PATH);
 
-router.get("/", (request, response) => {
-  response.send("cards-123456789");
+router.get("/:id", (request, response) => {
+  //response.send("cards-123456789");
   fsPromises
     .readFile(CARDS_PATH, { encoding: "utf8" })
-    .then((cards) => response.send({ data: JSON.parse(cards) }))
+    .then((cards) => {
+      response.send({ data: JSON.parse(cards) });
+    })
     .catch((error) =>
-      response.status(500).send({ message: `An error has occurred (${error})` })
+      response.send({ message: `An error has occurred (${error})` })
     );
 });
 
